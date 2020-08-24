a1c8a749bb927e02d59e1f1a84904b65f3d6604d
AdamManselll <adammansellhmb@gmail.com>
2020-08-24 13:29:04 -0700

    Add User Interface.
    Co-Authored by Grant Eadie <grantleadie@gmail.com>
    Co-authored-by: Kolman Soifer <kolmansoifer@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -46,3 +46,19 @@ Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
   // Add update method?
 }
+
+
+// User Interface Logic ---------
+let addressBook = new AddressBook();
+
+$(document).ready(function() {
+  $("form#new-contact").submit(function(event) {
+    event.preventDefault();
+    const inputtedFirstName = $("input#new-first-name").val();
+    const inputtedLastName = $("input#new-last-name").val();
+    const inputtedPhoneNumber = $("input#new-phone-number").val();
+    let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
+    addressBook.addContact(newContact);
+    console.log(addressBook.contacts);
+  })
+})
