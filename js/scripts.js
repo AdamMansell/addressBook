a98c1d25ed04dda8162afcd6dc21cd6cb9db5a66
AdamManselll <adammansellhmb@gmail.com>
2020-08-25 11:11:05 -0700

    Object Oriented js address book requirements done.
    Co-authored-by: Janet Karpenske <janetkarpenske@gmail.com>.
    Co-authored-by:  Kolman Soifer <kolmansoifer@gmail.com>.

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,7 @@ function AddressBook() {
     this.currentId = 0
 }
 
+
 AddressBook.prototype.addContact = function (contact) {
   contact.id = this.assignId();
   this.contacts.push(contact);
@@ -38,16 +39,14 @@ AddressBook.prototype.deleteContact = function (id) {
 }
 
 // Business Logic for Contacts ---------
-function Contact(firstName, lastName, phoneNumber, email) {
+function Contact(firstName, lastName, phoneNumber, email, workEmail, physicalAddress) {
   this.firstName = firstName,
     this.lastName = lastName,
     this.phoneNumber = phoneNumber,
-    this.email = email
+    this.email = [email, workEmail],
+    this.physicalAddress = physicalAddress
 }
 
-Contact.prototype.fullName = function () {
-  return this.firstName + " " + this.lastName;
-}
 
 // User Interface Logic ---------
 let addressBook = new AddressBook();
@@ -67,10 +66,18 @@ function showContact(contactId) {
   $(".first-name").html(contact.firstName);
   $(".last-name").html(contact.lastName);
   $(".phone-number").html(contact.phoneNumber);
-  $(".email").html(contact.email);
+  $(".email").html(contact.email[0]);
+  $(".work-email").html(contact.email[1]);
+  $(".physical-address").html(contact.physicalAddress);
   let buttons = $("#buttons");
+  let delButton1 = $("#delButton1");
+  let delButton2 = $("#delButton2");
   buttons.empty();
+  delButton1.empty();
+  delButton2.empty();
   buttons.append("<button class='deleteButton' id=" + contact.id + ">Delete</button>");
+  delButton1.append("<button class='deleteButton' id=" + contact.email[0] + ">Delete Email</button>")
+  delButton2.append("<button class='deleteButton' id=" + contact.email[1] + ">Delete Email</button>")
 }
 
 function attachContactListeners() {
@@ -82,6 +89,18 @@ function attachContactListeners() {
     $("#show-contact").hide();
     displayContactDetails(addressBook);
   });
+  $("#delButton1").on("click", ".deleteButton", function () {
+    $(".email").remove();
+    $("#email").hide();
+    $(".email").hide();
+    $("#delButton1").empty();
+  })
+  $("#delButton2").on("click", ".deleteButton", function () {
+    $(".work-email").remove();
+    $("#workEmail").hide();
+    $(".work-email").hide();
+    $("#delButton2").empty();
+  })
 };
 
 $(document).ready(function () {
@@ -92,12 +111,23 @@ $(document).ready(function () {
     const inputtedLastName = $("input#new-last-name").val();
     const inputtedPhoneNumber = $("input#new-phone-number").val();
     const inputtedEmail = $("input#new-email").val();
+    const inputtedWorkEmail = $("input#work-email").val();
+    const inputtedPhysicalAddress = $("input#new-physical-address").val();
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
     $("input#new-phone-number").val("");
     $("input#new-email").val("");
-    let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber, inputtedEmail);
+    $("input#work-email").val("");
+    $("input#new-physical-address").val("");
+    let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber, inputtedEmail, inputtedWorkEmail, inputtedPhysicalAddress);
     addressBook.addContact(newContact);
     displayContactDetails(addressBook);
+    if (inputtedEmail == "") {
+      $("#delButton1").hide();
+      $("#email").hide();
+    } if (inputtedWorkEmail == "") {
+      $("#workEmail").hide();
+      $("#delButton2").hide();
+    }
   })
 })
